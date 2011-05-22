f40396b8cd0e45315b79a7b0f425d51753d9d0bb
Jackworst <jackworst@gmail.com>
2011-05-22 19:52:11 +0200

    first commit

---
+++ exp.js
@@ -0,0 +1,75 @@
+var http = require("http"), url = require("url"), form = require("./lib/cb-forms"), mime = require("mime");
+var mongo = require('./lib/mongo-driver/node-mongodb-native/lib/mongodb');
+var fs = require("fs");
+
+var staticRe = /^\/static\/([\w\.]+)$/;
+
+var db = new mongo.Db('test', new mongo.Server("127.0.0.1", 27017, {}));
+
+var withExpenses = function(cb) {
+    db.open(function(err, p_client) {
+        db.collection('exp', function(err, expenses) {
+            if (err) {
+                throw err;
+            }
+            cb(expenses);
+        });
+    });
+};
+
+var sendResponse = function(response, data) {
+    response.writeHead(200, {'Content-Type': 'application/json'});
+    response.end(JSON.stringify(data) + "\n", "UTF-8");
+};
+
+var handleSync = function(request, response) {
+    form.onData(request, function(data) {
+        console.log("POST data: " + JSON.stringify(data));
+
+        var expense = {};
+        expense.eid = data.eid
+        expense.date = parseInt(data.date, 10);
+        expense.amount = parseInt(data.amount, 10);
+        expense.text = data.text || "";
+
+        withExpenses(function(expenses) {
+            expenses.find({eid:expense.eid}).toArray(function(e, existing) {
+                if (existing.length === 0) {
+                    expenses.insert(expense, function(err, docs) {
+                        sendResponse(response, {ok: true, eid: expense.eid});
+                    });
+                } else {
+                    sendResponse(response, {ok: false, error: "duplicates: " + JSON.stringify(existing)});
+                }
+            });
+        });
+    });
+};
+
+var handleStatic = function(request, response, resource) {
+    console.log("serving " + resource);
+    fs.readFile("./static/" + resource, function (err, data) {
+        if (!err) {
+            response.writeHead(200, {'Content-Type': mime.lookup(resource)});
+            response.end(data);
+        } else {
+            response.writeHead(404, {'Content-Type': 'text/plain'});
+            response.end("ERROR", "UTF-8");
+        }
+    });
+};
+
+http.createServer(function (request, response) {
+    var reqUrl = url.parse(request.url);
+
+    console.log("----- " + request.connection.remoteAddress + " " + reqUrl.href + " -----");
+    if (reqUrl.pathname === "/sync") {
+        handleSync(request, response);
+    } else if (staticRe.test(reqUrl.pathname)) {
+        handleStatic(request, response, staticRe.exec(reqUrl.pathname)[1]);
+    } else {
+        console.log("ignoring");
+        response.writeHead(404, {'Content-Type': 'text/plain'});
+        response.end();
+    }
+}).listen(1337);
---
+++ static/exp.js
@@ -0,0 +1,86 @@
+/* exp.js */
+
+var resetFields = function() {
+    $("#howMuch, #what").val(""),
+    $("#when").val(new Date());
+};
+
+var addExpense = function(expense) {
+    var expenses = JSON.parse(localStorage.expenses || "[]");
+    expenses.push(expense);
+    localStorage.expenses = JSON.stringify(expenses);
+};
+
+var clearExpense = function(eid) {
+    var expenses = JSON.parse(localStorage.expenses || "[]");
+    expenses = expenses.filter(function(ex){
+        return ex.eid !== eid;
+    });
+    localStorage.expenses = JSON.stringify(expenses);
+};
+
+var syncExpenses = function(cb) {
+    var expenses = JSON.parse(localStorage.expenses || "[]");
+    var todo = expenses.length;
+    $.each(expenses, function(i, expense) {
+        $.post("/sync", expense, function(response) {
+            if (response.ok) {
+                clearExpense(response.eid);
+                statusMessage("success", "OK: " + expense.eid);
+            } else {
+                alert("failed: " + expense.eid + "\n" + response.error);
+                statusMessage("failure", "ERR: " + expense.eid);
+            }
+            todo -= 1;
+            if (todo == 0) {
+                cb();
+            }
+        });
+    });
+};
+
+var listExpenses = function(target) {
+    var expenses = JSON.parse(localStorage.expenses || "[]");
+    var table = $('<table/>');
+    $.each(expenses, function(i, expense) {
+        var tr = $('<tr/>');
+        tr.append($('<td/>').text(new Date(expense.date * 1000).toString()));
+        tr.append($('<td/>').text(expense.amount));
+        tr.append($('<td/>').text(expense.text));
+        var delLink = $('<a href="#">Delete</a>').click(function() {
+            clearExpense(expense.eid);
+            listExpenses($("#list"));
+        });
+        tr.append($('<td/>').append(delLink));
+        table.append(tr);
+    });
+    target.empty().append(table);
+};
+
+var statusMessage = function(kind, text) {
+    $("#status").append($('<p/>').addClass(kind).text(text));
+};
+
+$(document).ready(function() {
+    $("#add").click(function() {
+        addExpense({
+            eid: "" + new Date().getTime() + "_" + Math.random(), 
+            date: Date.parse($("#when").val()) / 1000,
+            amount: $("#howMuch").val(),
+            text: $("#what").val(),
+        });
+        resetFields();
+        listExpenses($("#list"));
+        return false;
+    });
+
+    $("#sync").click(function() {
+        syncExpenses(function() {
+            listExpenses($("#list"));
+        });
+        return false;
+    });
+
+    resetFields();
+    listExpenses($("#list"));
+});
